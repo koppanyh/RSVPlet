c503407821429f2faef7f6a5a15eab3ed1cea7b9
Koppany Horvath <koppanyh12@gmail.com>
2022-08-22 23:45:47 -0700

    Started the refactor

--- source.js
+++ source.js
@@ -1,64 +1,59 @@
-javascript: var settings = {wpm: 400, startDelay: 2000, punctMult: 2.5, punctAdd: 0, symbMult: 1.5, symbAdd: 0, fontSize: "50px", color: "white", color2: "#FF4444", backgroundColor: "rgba(0, 0, 0, 0.9)", punctuation: ".,!?;", symbols: "0123456789`~@#$%^&*()-_=+[{]}\\|:'\"<>/"};
-/* TODO get selection working within PDFs and box of https://developer.mozilla.org/en-US/docs/Web/CSS/vertical-align */
-var sel = window.getSelection? window.getSelection() : document.getSelection? document.getSelection() : document.selection.createRange().text;
-var words = (sel + '').replace(/\n/g, '  ').split(' ').reverse();
-function includeAny(word, chars) {
-    for (var i = 0; i < chars.length; i++)
-        if (word.includes(chars[i]))
-            return true;
-    return false;
-}
-var charDelay = 60000 / (settings.wpm * 5);
-function calcDelay(word) {
-    var delay = Math.max(word.length, 5) * charDelay;
-    if (includeAny(word, settings.punctuation))
-        delay = (delay * settings.punctMult) + settings.punctAdd;
-    else if (includeAny(word, settings.symbols))
-        delay = (delay * settings.symbMult) + settings.symbAdd;
-    return delay;
-}
-var rsvpDiv;
-/* TODO get it to pause somehow and go away if you need to */
-function setupDiv() {
-    rsvpDiv = document.createElement("div");
-    rsvpDiv.style.position = "fixed";
-    rsvpDiv.style.top = "40%";
-    rsvpDiv.style.left = "10%";
-    rsvpDiv.style.width = "80%";
-    rsvpDiv.style.padding = "0.5em";
-    rsvpDiv.style.borderRadius = "10px";
-    rsvpDiv.style.textAlign = "center";
-    rsvpDiv.style.fontWeight = "bold";
-    rsvpDiv.style.fontSize = settings.fontSize;
-    rsvpDiv.style.fontFamily = "monospace";
-    rsvpDiv.style.color = settings.color;
-    rsvpDiv.style.backgroundColor = settings.backgroundColor;
-    rsvpDiv.style.zIndex = 999999999;
-    document.body.appendChild(rsvpDiv);
-}
-function printWord(word) {
-    if (word == ' ' || word == '') {
-        rsvpDiv.innerHTML = "&nbsp";
-        return;
-    }
-    var middle = Math.floor(word.length / 2);
-    var first = word.substr(0, middle);
-    var second = word.substr(middle, 1);
-    var third = word.substr(middle + 1);
-    rsvpDiv.innerHTML = `${first}<span style="color: ${settings.color2}">${second}</span>${third}`;
-}
-/* TODO if the word is the same as the last one, blink it */
-function rsvp() {
-    if (!words.length) {
-        rsvpDiv.remove();
-        return;
-    }
-    var word = words.pop();
-    printWord(word);
-    setTimeout(rsvp, calcDelay(word));
-}
-setupDiv();
-printWord(words.pop());
-setTimeout(rsvp, settings.startDelay);
-/* TODO add estimated reading time */
-/* TODO add progress bar */
+javascript: (function() {
+	const settings = {wpm: 400, startDelay: 2000, punctMult: 2.5, punctAdd: 0, symbMult: 1.5, symbAdd: 0, fontSize: "50px", color: "white", color2: "#FF4444", backgroundColor: "rgba(0, 0, 0, 0.9)", punctuation: ".,!?;:", symbols: "0123456789`~@#$%^&*()-_=+[{]}\\|'\"<>/"};
+	function includeAny(word, chars) {
+		for (let i = 0; i < chars.length; i++)
+			if (word.includes(chars[i]))
+				return true;
+		return false;
+	}
+	const charDelay = 60000 / (settings.wpm * 5);
+	function calcDelay(word) {
+		let delay = Math.max(word.length, 5) * charDelay;
+		if (includeAny(word, settings.punctuation))
+			delay = (delay * settings.punctMult) + settings.punctAdd;
+		else if (includeAny(word, settings.symbols))
+			delay = (delay * settings.symbMult) + settings.symbAdd;
+		return delay;
+	}
+	let RSVP = class {
+		constructor(text) {
+			let splitted = text.trim().replace(/\n/g, '  ').split(' ');
+			this.words = splitted.map(x => {
+				return {text: x, delay: calcDelay(x)};
+			});
+			this.index = 0;
+			/* this.totalTime = words.map(x => x.delay)
+				.reduce((a, b) => a + b, 0); */
+			this.timeout = null;
+		}
+		next() {
+			if (this.index >= this.words.length)
+				return null;
+			return this.words[this.index++];
+		}
+		run() {
+			let next = this.next();
+			if (!next)
+				return;
+			this.timeout = setTimeout(() => { this.run(); },
+				next.delay);
+			console.log(next.text);
+		}
+		start() {
+			let next = this.next();
+			this.timeout = setTimeout(() => { this.run(); },
+				settings.startDelay);
+			console.log(next.text);
+		}
+		stop() {
+			clearInterval(this.timeout);
+		}
+	};
+	let selection = (
+		window.getSelection ? window.getSelection() :
+		document.getSelection ? document.getSelection() :
+		document.selection.createRange().text
+	) + '';
+	let rsvp = new RSVP(selection);
+	rsvp.start();
+})();
